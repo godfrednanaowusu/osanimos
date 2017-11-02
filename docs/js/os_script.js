629d943b692423f3963907a3dd61420bc24aa746
Godfred <iamgodfredowusu@gmail.com>
2017-11-02 11:40:59 +0000

    a

--- docs/js/os_script.js
+++ docs/js/os_script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(e){
     
     $('#startbutton').on('click', function(e){
-        $('#appcontainerhoder').Toggle();
+        $('#appcontainerhoder').toggle();
     });
     
 });

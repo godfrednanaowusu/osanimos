ae2a27d30a53961b422d910dfeb79e4f2e727ea4
Godfred <iamgodfredowusu@gmail.com>
2017-11-02 18:04:11 +0000

    a

--- docs/js/os_script.js
+++ docs/js/os_script.js
@@ -16,7 +16,7 @@ function goto_app(path){
         $('#windowframe').show();
         $('#windowframe').empty();
 
-        $('#windowframe').append('<iframe src="'+path+'" style="width:100%; height:100%; border:none;"></iframe>');
+        $('#windowframe').append('<iframe src="'+path+'" style="width:100%; height:100%; border:none; position:absolute; top:0; right:0; bottom:0; left:0;"></iframe>');
         $('#windowframe').append('<div class="bottombar"><img src="images/android_gear.png" onclick="close_app()" style="height:100%; display:block; float:left; margin:0 3px;"/></div>')
     
 }

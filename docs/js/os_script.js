9b41a0676f57bb61693f9ba5ad0d946c2f986e07
Godfred <iamgodfredowusu@gmail.com>
2017-11-02 11:17:22 +0000

    a

--- docs/js/os_script.js
+++ docs/js/os_script.js
@@ -3,4 +3,7 @@ $(document).ready(function(e){
     $('#startbutton').on('click', function(e){
         $('#appcontainerhoder').slideToggle();
     });
+    $('#appcontainerhoder').focusout(function () {
+        $('#appcontainerhoder').slideDown();
+    });
 });

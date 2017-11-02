eefe2f8d2f4d0e38bddfcedd01d064b8cbd02257
Godfred <iamgodfredowusu@gmail.com>
2017-11-02 10:45:33 +0000

    a

--- docs/js/os_script.js
+++ docs/js/os_script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(e){
     
-    $('#startbutton').on('on', function(e){        
+    $('#startbutton').on('touchstart', function(e){        
         alert();
         // $('#appcontainerhoder').addClass('animated slideInUp');        
         //     //wait for animation to finish before removing classes
@@ -11,4 +11,4 @@ $(document).ready(function(e){
         
         // $('#appcontainerhoder').slideToggle("fast", "swing");
     });
-})
+});

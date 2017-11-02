fea49a78a76885da576e625b1d6e85901c09ea1e
Godfred <iamgodfredowusu@gmail.com>
2017-11-02 10:12:06 +0000

    b

--- docs/js/os_script.js
+++ docs/js/os_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(e){
     $('#startbutton').on('click', function(e){
-        $('#appcontainerhoder').slideToggle();
+        $('#appcontainerhoder').slideToggle("swing");
     })
 })

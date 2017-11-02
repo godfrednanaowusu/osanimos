e545a53d483349e6bf6df6dbe062c29df85a33ea
Godfred <iamgodfredowusu@gmail.com>
2017-11-02 10:47:38 +0000

    a

--- docs/js/os_script.js
+++ docs/js/os_script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(e){
     
     $('#startbutton').on('touchstart', function(e){        
-        alert();
-        // $('#appcontainerhoder').addClass('animated slideInUp');        
-        //     //wait for animation to finish before removing classes
-        //     window.setTimeout( function(){
-        //         $('#appcontainerhoder').removeClass('animated slideInUp');
-        //     }, 2000);         
+        
+        $('#appcontainerhoder').addClass('animated slideInUp');        
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                $('#appcontainerhoder').removeClass('animated slideInUp');
+            }, 2000);         
   
         
         // $('#appcontainerhoder').slideToggle("fast", "swing");

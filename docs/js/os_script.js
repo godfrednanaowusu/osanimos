7996854a9b98b230fad0c835601edf4fae1bf59f
Godfred Nana Owusu <32749058+godfrednanaowusu@users.noreply.github.com>
2018-03-26 15:58:14 +0000

    Add files via upload

--- docs/js/os_script.js
+++ docs/js/os_script.js
@@ -1,3 +1,8 @@
+var cascadestatus = 'true';
+var zindex = 100;
+
+
+
 $(document).ready(function(e){
     $('#base').show();
 
@@ -21,45 +26,96 @@ $(document).ready(function(e){
         $('#bottomrightbar').show();
      }
 
-     $('.titlebar').click(function(e){
-alert();
+     $('#mainbar').on('click', function(e){
+        $('.taskbarholders').hide();
      });
 
-    
-    // $('#startbutton').on('touchstart', function(e){
-    //     $('#appcontainerhoder').slideToggle();
-    // });
 
-    // $('div.icon').on('click', function(e){
-    //     $('#appcontainerhoder').hide();
-    //     $('.windowframe').show();
-    // });
+    //  BATTERY FUNCTIONS
     
+     navigator.getBattery().then((battery) => {
+        $('.batterypercentage').text(Math.round(battery.level * 100)+ '%');
+         battery.addEventListener('levelchange', function(){
+            $('.batterypercentage').text(Math.round(battery.level * 100)+ '%');
+         });
+         battery.ondischargingtimechange = (event) =>{
+            $('.batterypercentage').text(Math.round(event.target.level * 100)+ '%');
+         }
+         battery.onchargingtimechange = (event) =>{
+            $('.batterypercentage').text(Math.round(event.target.level * 100)+ '%');
+        }
+
+     });
+     
+
+     
+
 });
 
 function toggleStartMenu(){
-    $('#appcontainerhoder').slideToggle('fast');
-}
-
-function goto_app(path){
-        $('#appcontainerhoder').hide();
-        $('#windowframe').show();
-        $('#windowframe').empty();
-
-        $('#windowframe').append('<iframe src="'+path+'" style="width:100%; height:calc(100% - 20px); border:none; position:absolute; top:20px; right:0; bottom:0; left:0;"></iframe>');
-        $('#windowframe').append('<div class="titlebar" draggable="true"><p id="title">Window Frame</p><button id="closebut" type="button" onclick="close_app()" ></button> <button type="button" id="cascadebut" onclick="cascade_app()"></button> <button type="button" id="maximizebut" onclick="close_app()"></button>  </div>')
+    var uid = new Date().valueOf();
+    $('#appcontainerholder').slideToggle('fast');
+    $('#appcontainerholder').css('z-index', uid);
     
 }
 
-function cascade_app(){
-    // $('#appcontainerhoder').hide();
-    $('#windowframe').css({width:'500px', height:'400px', 'margin':'5% auto 0'});
-    // $('#windowframe').hide();
+function togglePowerMenu(){
+    var uid = new Date().valueOf();
+    $('#powercontainerholder').slideToggle('fast');
+    $('#powercontainerholder').css('z-index', uid);
     
 }
-function close_app(){
-    $('#appcontainerhoder').hide();
-    $('#windowframe').empty();
-    $('#windowframe').hide();
-    
+
+function goto_app(path, title){
+        $('#appcontainerholder').hide();        
+        var uid = new Date().valueOf();
+        var windowid = 'windowframe_'+uid;
+        $('#mainbar').append('<div id="'+windowid+'" class="windowframe ui-widget-content" ondragstart="onwindowdrag('+windowid+')" onmousedown="onwindowclick('+windowid+')" ondblclick="onwindowdblclick('+windowid+')" style="position:absolute; z-index:'+parseInt(uid)+'"> </div>');
+        $('#windowframe_'+uid).append('<iframe src="'+path+'" style="width:calc(100% - 2px); height:calc(100% - 21px); border:none; position:absolute; top:20px; right:1px; bottom:1px; left:1px; background:#fff; "></iframe>');
+        $('#windowframe_'+uid).append('<div class="titlebar" ><p id="title">'+title+'</p><button id="closebut" type="button" onclick="close_app('+windowid+')" ></button> <button type="button" id="cascadebut" onclick="cascade_app('+windowid+');"></button> <button type="button" id="minimizebut" onclick="minimize_app('+windowid+')"></button>  </div>');
+        
+       
+        
+}
+
+
+function cascade_app(windowid){
+    var uid = new Date().valueOf();
+    if(cascadestatus == 'true'){
+        $(windowid).css({width:'500px', height:'400px', position:'absolute',  left:'35%', top:'15%', 'z-index':uid});
+        $(windowid).removeClass('windowmaximised');
+        $(windowid).addClass('windowcascaded');
+        cascadestatus = 'false';
+    }else{
+        $(windowid).css({width:'100%', height:'100%', position:'absolute', left:'0px', top:'0px', right:'0px', 'z-index':uid});
+        $(windowid).removeClass('windowcascaded');
+        $(windowid).addClass('windowmaximised');
+        cascadestatus = 'true';
+    }   
+}
+
+function minimize_app(windowid){
+    cascade_app(windowid); 
+}
+
+function close_app(windowid){
+    $('#appcontainerholder').hide();
+    $(windowid).empty();
+    $(windowid).remove();    
+}
+
+function onwindowdrag(windowid){
+    $(windowid).removeClass('windowcascaded');
+    $(windowid).removeClass('windowmaximised');
+    $(windowid).draggable({containment:'', iframeFix: true});
+    $(windowid).resizable();
+}
+
+function onwindowclick(windowid){    
+    var uid = new Date().valueOf();    
+    $(windowid).css({'z-index': parseInt(uid)});
+}
+
+function onwindowdblclick(windowid){
+    cascade_app(windowid);
 }
